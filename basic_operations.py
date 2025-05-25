a85baa9d943127d285d7aca24867051b794b4c9c
surafelmengist <surafelmengist2015@gmail.com>
2025-05-25 13:00:09 +0300

    python first comment

---
+++ basic_operations.py
@@ -0,0 +1,20 @@
+# number1=10
+# number2=5
+# print("addation of " + number1  + "and" + number2 + "is" +str( number1 +number2))
+# print("subtraction of " + number1 + "and " + number2  + "is "  +str ( number1-number2))
+# print("multiplication of  " +number1 + "and " + number2 + "is " + str (number1 * number2))
+
+
+
+a = 10
+b = 5
+
+# Perform operations
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+
+# Print results
+print(f"Addition of {a} and {b} is {addition}")
+print(f"Subtraction of {a} and {b} is {subtraction}")
+print(f"Multiplication of {a} and {b} is {multiplication}")
